37fc94e106d7a56f2f3255d669a5f0ca8e459756
Haytham Mohamed <haybu@hotmail.com>
2022-05-17 16:35:54 -0500

    update

--- cluster/main.tf
+++ cluster/main.tf
@@ -23,7 +23,7 @@ resource ibm_container_vpc_cluster cluster {
     }
   }
 
-  disable_public_service_endpoint = false
+  disable_public_service_endpoint = true
 
   # kms_config {
   #   instance_id      = var.key_id
